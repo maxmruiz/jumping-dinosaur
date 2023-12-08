7c2989168e075edb6a62c951ab537a8626380196
max <mmfern2003@gmail.com>
2023-12-08 16:30:01 -0500

    game config added via phaser

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const canvas = document.getElementbyId('canvas');
 const c = canvas.getContext('2d');
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'canvas',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y : 300 },
+            debug: true
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
